9bb22dbabf12bbb409d5317cb7a3bc5a271b2d95
muisbuys <matthew.buys@gmail.com>
2015-06-25 23:08:32 +0200

    Initial Commit

---
+++ src/companysharesapp/CompanySharesApp.java
@@ -0,0 +1,15 @@
+//Matthew Buys
+package companysharesapp;
+
+import javax.swing.JScrollPane;
+import org.jgraph.JGraph;
+
+public class CompanySharesApp {
+
+    public static void main(String[] args) 
+    {
+        JGraph graph = new JGraph();
+        JScrollPane scrollPane = new JScrollPane(graph);
+
+    }    
+}
