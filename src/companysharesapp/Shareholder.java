6ee8a148e919ce1db7b5804d44342c7a95e26ab0
muisbuys <matthew.buys@gmail.com>
2015-07-02 14:51:24 +0200

    Finished Shareholders Class
    Overloaded the class and completed the getters and setters

--- src/companysharesapp/Shareholder.java
+++ src/companysharesapp/Shareholder.java
@@ -1,3 +1,4 @@
+//Matthew Buys
 package companysharesapp;
 
 import java.sql.Connection;
@@ -9,15 +10,32 @@ import java.sql.Statement;
 public class Shareholder 
 {
     private Connection conn;
+    private Statement stmt;
+    private void Connection()//method for the connection to DB
+    {
+        try 
+        {
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CompanySharesDB", "nbuser", "nbuser");//Connection to DB
+            stmt = conn.createStatement();
+        } catch (SQLException ex) 
+        {
+            System.out.println("Could not estalish a Connection via Shareholder-Connection method");
+        }
+    }
+    private void ExecuteSQL(String sql)
+    {
+        try {
+            stmt.execute(sql);
+        } catch (SQLException ex) {
+            System.out.println("Error executing SQL via ExecuteSQL method");
+        }
+    }
     int id;int address;String name;String number;String dateOfBirth;int totalShares;
     public Shareholder(int id)//for when the ID is known
     {
         try {
-            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/CompanySharesDB", "nbuser", "nbuser");//Connection to DB
-            System.out.println("Connection to CompanySharesDB Database Established");
-            Statement stmt;
-            stmt = conn.createStatement();
-            String sql = "select * from NBUSER.\"Shareholder_Details\"\n WHERE \"Shareholder_ID\" = "+id;//sql query
+            Connection();
+            String sql = "select * from \"Shareholder_Details\"\n WHERE \"Shareholder_ID\" = "+id;//sql query
             ResultSet rs = stmt.executeQuery(sql);
             rs.next();//skips first line
             this.id =id;//populates class variables
@@ -33,7 +51,21 @@ public class Shareholder
     }
     public Shareholder(String name)//for when the name is known
     {
-        this.name = name;
+        try {
+            Connection();
+            String sql = "select * from \"Shareholder_Details\"\n WHERE \"Shareholder_Name\" = '"+name+"'";//sql querry
+            ResultSet rs = stmt.executeQuery(sql);
+            rs.next();//skips first line
+            this.name = name;
+            id = rs.getInt(1);
+            address = rs.getInt(2);
+            number = rs.getString(4);
+            dateOfBirth = ""+rs.getDate(5);
+            totalShares = rs.getInt(6);
+        } catch (SQLException ex) 
+        {
+            System.out.println("Could not establish a Name-Shareholder connection");//to know where the error is.
+        }
     }
 
     public int getId() 
@@ -41,8 +73,14 @@ public class Shareholder
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id) 
+    {
         this.id = id;
+        Connection();
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Shareholder_ID\" = "+id+"\n" +
+                    "WHERE \"Shareholder_Name\" = '"+name+"'";
+        ExecuteSQL(sql);
     }
 
     public int getAddressID() {
@@ -51,6 +89,10 @@ public class Shareholder
 
     public void setAddressID(int address) {
         this.address = address;
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Address_ID\" = "+address+"\n" +
+                    "WHERE \"Shareholder_ID\" = '"+id+"'";
+        ExecuteSQL(sql);
     }
 
     public String getName() {
@@ -59,6 +101,10 @@ public class Shareholder
 
     public void setName(String name) {
         this.name = name;
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Shareholder_Name\" = "+name+"\n" +
+                    "WHERE \"Shareholder_ID\" = '"+id+"'";
+        ExecuteSQL(sql);
     }
 
     public String getNumber() {
@@ -67,6 +113,10 @@ public class Shareholder
 
     public void setNumber(String number) {
         this.number = number;
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Shareholder_Number\" = "+number+"\n" +
+                    "WHERE \"Shareholder_ID\" = '"+id+"'";
+        ExecuteSQL(sql);
     }
 
     public String getDateOfBirth() {
@@ -75,6 +125,10 @@ public class Shareholder
 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Shareholder_DOB\" = "+dateOfBirth+"\n" +
+                    "WHERE \"Shareholder_ID\" = '"+id+"'";
+        ExecuteSQL(sql);
     }
 
     public int getTotalShares() {
@@ -83,6 +137,10 @@ public class Shareholder
 
     public void setTotalShares(int totalShares) {
         this.totalShares = totalShares;
+        String sql = "UPDATE \"Shareholder_Details\"\n" + //sql querry
+                    "SET \"Shareholder_TotalShares\" = "+totalShares+"\n" +
+                    "WHERE \"Shareholder_ID\" = '"+id+"'";
+        ExecuteSQL(sql);
     }
     
 }
